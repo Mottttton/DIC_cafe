64e242f7e9402736ab4b446af87cb412d63cd229
Mottttton <mottttton.webdev@gmail.com>
2024-01-12 16:37:59 +0900

    送料の条件式追加

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -14,10 +14,18 @@ function add() {
 
 function calc() {
   let sum = 0;
+  let postage;
   for(let i = 0; i < purchases.length; i++) {
     sum += purchases[i].price * purchases[i].quantity;
   }
-  window.alert(`合計は${sum}円です。`)
+  if(sum >= 3000 || sum === 0) {
+    postage = 0;
+  } else if(sum >= 2000) {
+    postage = 250;
+  } else {
+    postage = 500
+  }
+  window.alert(`送料は${postage}円です。合計は${sum + postage}円です。`)
   purchases = [];
   priceElement.value = "";
   quantityElement.value = "";
