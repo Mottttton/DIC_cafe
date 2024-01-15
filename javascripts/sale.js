3d85cce7d1804e8f086df02e7203bbca2b67129e
Mottttton <mottttton.webdev@gmail.com>
2024-01-15 14:20:43 +0900

    小計の表示方法を修正

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -10,23 +10,45 @@ function add() {
     quantity: qty,
   };
   purchases.push(purchase);
+  alert(display(purchases));
+}
+
+function display(cartItems) {
+  let string = "";
+  for (cartItem of cartItems) {
+    string += `${cartItem.price}円が${cartItem.quantity}点\n`;
+  }
+  string += "\n小計は" + subtotal(cartItems) + "円です";
+  return string;
+}
+
+function subtotal(cartItems) {
+  let currentSum = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    currentSum += cartItems[i].price * cartItems[i].quantity;
+  }
+  return currentSum;
 }
 
 function calc() {
   let sum = 0;
   let postage;
-  for(let i = 0; i < purchases.length; i++) {
-    sum += purchases[i].price * purchases[i].quantity;
-  }
-  if(sum >= 3000 || sum === 0) {
-    postage = 0;
-  } else if(sum >= 2000) {
-    postage = 250;
-  } else {
-    postage = 500
-  }
-  window.alert(`送料は${postage}円です。合計は${sum + postage}円です。`)
+
+  sum = subtotal(purchases);
+  postage = calcPostageFromPurchase(sum);
+
+  window.alert(`送料は${postage}円です。合計は${sum + postage}円です。`);
   purchases = [];
   priceElement.value = "";
   quantityElement.value = "";
+}
+
+function calcPostageFromPurchase(purchasePrice){
+  if (purchasePrice >= 3000 || purchasePrice === 0) {
+    return postage = 0;
+  } else if (purchasePrice >= 2000) {
+    return postage = 250;
+  } else {
+    return postage = 500;
+  }
 }
