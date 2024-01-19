75912a5c094948d89114c52a992d02617c02a659
Mottttton <mottttton.webdev@gmail.com>
2024-01-19 16:29:44 +0900

    submit task: form varidation

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,18 +1,25 @@
 function emailValidation() {
   const form = document.getElementById("form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (form.email.value !== form.email_confirm.value) {
-      const validationFailMessage = document.createElement("p");
-      const message = document.createTextNode("Eメールが一致しません");
-      form.appendChild(validationFailMessage);
-      validationFailMessage.appendChild(message);
-      validationFailMessage.classList.add("alert");
-      setTimeout(function () {
-        form.removeChild(validationFailMessage);
-      }, 3000);
+  const formContactLine = document.getElementsByClassName("contact_line");
+  const parentElement = formContactLine[0].parentElement;
+  const formEmailConfirm = document.getElementById("email_confirm");
+  const validationFailMessage = document.createElement("p");
+
+  validationFailMessage.setAttribute("id", "alert");
+  validationFailMessage.classList.add("alert_color");
+  const message = document.createTextNode("Eメールが一致しません");
+  validationFailMessage.appendChild(message);
+
+  formEmailConfirm.addEventListener("input", (e) => {
+    let isEmailConfirmed = form.email.value === form.email_confirm.value
+    if(!isEmailConfirmed && document.getElementById("alert")) {
+      
+    } else if (!isEmailConfirmed) {
+      parentElement.insertBefore(validationFailMessage, parentElement.children[3]);
+      formEmailConfirm.classList.add("alert_textarea");
     } else {
-      form.submit();
+      parentElement.removeChild(validationFailMessage);
+      formEmailConfirm.classList.remove("alert_textarea");
     }
   });
 }
